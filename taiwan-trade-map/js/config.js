ee453607ffa254fe7f8be0f520a6c23b668916e2
Mariel de la Garza <mariel.code@gmail.com>
2024-10-08 11:22:28 -0400

    remove lorem ipsum text from chapter 1

--- taiwan-trade-map/js/config.js
+++ taiwan-trade-map/js/config.js
@@ -20,8 +20,7 @@ var config = {
       hidden: true,
       title: "Slide 1",
       image: "",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "",
       location: {
         center: [120.74709, 23.97749],
         zoom: 6.5,
