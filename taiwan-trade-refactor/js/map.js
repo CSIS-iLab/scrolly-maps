fa92f1d54bfaff0e11881fe3b78873897a8ab41b
FabioMurgia <123178334+FabioMurgia@users.noreply.github.com>
2024-10-10 00:06:35 -0400

    Update map.js

--- taiwan-trade-refactor/js/map.js
+++ taiwan-trade-refactor/js/map.js
@@ -140,15 +140,15 @@ function updateChapterLocations(isMobile) {
         chapter.location.center = [120.74709, 23.97749];
       }
       if (chapter.id === "chapter2") {
-        chapter.location.zoom = 4;
+        chapter.location.zoom = 3;
         chapter.location.center = [120.74709, 23.97749];
       }
       if (chapter.id === "chapter3") {
-        chapter.location.zoom = 4;
+        chapter.location.zoom = 3;
         chapter.location.center = [111.74206, 14.93303];
       }
       if (chapter.id === "chapter4") {
-        chapter.location.zoom = 4.5;
+        chapter.location.zoom = 4;
         chapter.location.center = [120.74709, 23.97749];
       }
       if (chapter.id === "chapter5") {
